459c908b7c670d6b9b6cfc22ae3114894fcfe6e6
vincentvargas <harvathsteven@gmail.com>
2014-04-12 09:45:18 -0700

    server.js mods

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 /**
  * Created by harva_000 on 4/12/14.
  */
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('thesis.html');
-var port = Number(process.env.PORT || 5000);
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+port = Number(process.env.PORT || 5000);
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(index);
-}).listen(port);
+http.createServer(function(request, response) {
+
+    var uri = url.parse(request.url).pathname
+        , filename = path.join(process.cwd(), uri);
+
+    path.exists(filename, function(exists) {
+        if(!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+        fs.readFile(filename, "binary", function(err, file) {
+            if(err) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
