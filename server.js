532d840e6cf708474a0d8bc6baaa25709384b41e
vincentvargas <harvathsteven@gmail.com>
2014-04-12 09:35:41 -0700

    modding port

--- server.js
+++ server.js
@@ -4,8 +4,9 @@
 var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('thesis.html');
+var port = Number(process.env.PORT || 5000);
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(index);
-}).listen(9615);
+}).listen(port);
